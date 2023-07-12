2695bd66031e3a671abb4172fdadbc73dad92b2b
SplendorSA <130695828+SplendorSA@users.noreply.github.com>
2023-07-12 13:20:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-const http = new XMLHttpRequest()
-let result = document.querySelector("#result")
+function geoFindMe() {
+  const status = document.querySelector("#status");
+  const mapLink = document.querySelector("#map-link");
 
-document.querySelector("#share").addEventListener
-("click", () => {
-  findMyCoordinates()
-})
+  mapLink.href = "";
+  mapLink.textContent = "";
 
-function findMyCoordinates(){
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
 
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition
-    ((position) => {
-      const bdcAPI = 'https://api-bdc.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}'
-      getAPI(bdcAPI)
-    },
-    (err) => {
-      alert(err.message)
-    })
+    status.textContent = "";
+    mapLink.href = `https://www.google.com/maps/?hl=en/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = "Unable to retrieve your location";
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocation is not supported by your browser";
   } else {
-    alert("Geolocation is not supported by your browser")
+    status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
   }
 }
-function getAPI(bdcAPI){
-  http.open("GET", bdcAPI)
-  http.send()
-  http.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-      result.innerHTML = this.responseText
-    }
-  }
-}
+
+document.querySelector("#find-me").addEventListener("click", geoFindMe);
